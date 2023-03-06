32093c47ca4796d143b9f3be968185c155d94f3e
atesz014 <atesz014@gmail.com>
2023-03-06 16:40:39 +0100

    init

---
+++ BlazorTailwindDashboard/Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorTailwindDashboard.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Link { get; set; }
+    }
+}
